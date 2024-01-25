82c97ab7aaedae01858ecf98874951ae1a480844
IngAguilar <146897097+IngAguilar@users.noreply.github.com>
2024-01-24 20:57:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello new World 2');
+  //res.send('Hello new World 2');
+  res.send(process.env.HOSTNAME);
 })
-
-export default {  
-        data(){
-            return{
-                displayPodName:""
-        }
-    },
-    mounted(){
-        this.displayPodName=process.env.HOSTNAME
-      app.get('/', (req, res) => {
-        res.send(process.env.HOSTNAME);
-})
-        
-     }
-}
 
 module.exports = app;
