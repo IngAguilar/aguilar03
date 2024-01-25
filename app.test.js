5d00221da5e4008ae023419b3b96a43de934b797
IngAguilar <146897097+IngAguilar@users.noreply.github.com>
2024-01-24 20:12:49 -0600

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -11,5 +11,7 @@ describe('GET /', () => {
     // Espera que el status sea 200 y el texto sea 'Hello World2'
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('Hello new World 2');
+    expect(ip addr | awk '/inet/{print $2}' | awk -F/ '{print $1}') > /tmp/internal_ip.txt
+local_node=$(kubectl get node -o wide | grep -f /tmp/internal_ip.txt | awk '{print $1}');
   });
 });
